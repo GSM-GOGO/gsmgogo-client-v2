85c541927e71d3b4deccd5821ea249d9a3908d2e
Ethen1264 <jxorhks86909763@gmail.com>
2024-03-17 02:02:46 +0900

    add SigninPublishing

--- src/Pages/Signin/style.ts
+++ src/Pages/Signin/style.ts
@@ -1,9 +1,32 @@
 import styled from 'styled-components';
 
-export const TestTitle = styled.h1`
-  font-family: Pretendard;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
+export const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #202124;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Container = styled.div`
+  width: 46.875rem;
+  height: 40.625rem;
+`;
+
+export const ContainerResponse = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 52px;
+`;
+
+export const ImgBox = styled.img`
+  width: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
---
+++ src/assets/png/index.ts
@@ -0,0 +1,1 @@
+export { default as SignInLogo } from './SignInLogo.png';

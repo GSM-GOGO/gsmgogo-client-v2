f2ab33490146c5154aeace80faa63148fac56cd8
정태관 <ethen1264@jeongtaegwan-ui-MacBookPro.local>
2024-03-18 23:02:47 +0900

    fix signinPublishing

---
+++ src/utils/env.ts
@@ -0,0 +1,2 @@
+export const GAUTH_CLIENT_ID: string = '83826b547e88434eb1a6dfc27c2ba7f9d877615fc09a438c8703fbbea15a202e';
+export const REDIRECT_URI: string = 'http://localhost:8080/auth/callback';

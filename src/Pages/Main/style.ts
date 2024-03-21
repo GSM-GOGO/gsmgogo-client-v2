2b5a0aa1fdbe3f302bb252c5b2ed0c16ab644a86
gjaegyun <jaegyungim06@gmail.com>
2024-03-21 15:57:59 +0900

    Add Storybook

--- src/Pages/Main/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const TestTitle = styled.h1`
-  font-family: Pretendard;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-`;

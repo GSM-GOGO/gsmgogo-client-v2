4309ab64a2161cfc8c23150d594446d00463cecd
gjaegyun <jaegyungim06@gmail.com>
2024-03-22 00:14:57 +0900

    Change PageSettings

--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -1,9 +1,14 @@
 import HeaderContainer from '../../components/HeaderContainer/index.tsx';
+import PlayingContainer from '../../components/PlayingContainer/index.tsx';
+import * as S from "./style";
 
 const Main = () => {
   return (
     <>
       <HeaderContainer/>
+      <S.MainContainer>
+        <PlayingContainer/>
+      </S.MainContainer>
     </>
   );
 };

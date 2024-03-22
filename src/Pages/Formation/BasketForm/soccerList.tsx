528e8a9a997b7eaa4ae02a77dc5002684ccfd611
gjaegyun <jaegyungim06@gmail.com>
2024-03-22 11:15:20 +0900

    Add PlayerMokData

---
+++ src/Pages/Formation/BasketForm/soccerList.tsx
@@ -0,0 +1,57 @@
+type Players = {
+  id: number;
+  name: string;
+  x: number;
+  y: number;
+}
+
+export const playersList: Players[] = [
+  {
+      id: 1,
+      name: "1111 홍길동",
+      x: 0,
+      y: 0,
+  },
+  {
+      id: 2,
+      name: "1112 홍길동",
+      x: 0,
+      y: 0,
+  },
+  {
+      id: 3,
+      name: "1113 홍길동",
+      x: 0,
+      y: 0,
+  },
+  {
+      id: 4,
+      name: "1114 홍길동",
+      x: 0,
+      y: 0,
+  },
+  {
+      id: 5,
+      name: "1115 홍길동",
+      x: 600,
+      y: -900,
+  },
+  {
+      id: 6,
+      name: "1116 홍길동",
+      x: 0,
+      y: 0,
+  },
+  {
+      id: 7,
+      name: "1117 홍길동",
+      x: 0,
+      y: 0,
+  },
+  {
+      id: 8,
+      name: "1118 홍길동",
+      x: 0,
+      y: 0,
+  }
+];

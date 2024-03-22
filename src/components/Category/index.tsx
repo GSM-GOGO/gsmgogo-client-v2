e6a64c89df51fb1d47cd514b3a28415d7e3296d4
gjaegyun <jaegyungim06@gmail.com>
2024-03-23 02:56:50 +0900

    Change nullToUndefined

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -6,16 +6,16 @@ const Category = () => {
   const currentPath = location.pathname;
   return (
     <S.CategoryContainer>
-      <S.CategoryLink to="/" style={currentPath === '/' ? { color: '#fff' } : null}>
+      <S.CategoryLink to="/" style={currentPath === '/' ? { color: '#fff' } : undefined}>
         경기
       </S.CategoryLink>
-      <S.CategoryLink to="/matches/soccer" style={currentPath === '/matches/soccer' ? { color: '#fff' } : null}>
+      <S.CategoryLink to="/matches/soccer" style={currentPath === '/matches/soccer' ? { color: '#fff' } : undefined}>
         축구
       </S.CategoryLink>
-      <S.CategoryLink to="/matches/basketball" style={currentPath === '/matches/basketball' ? { color: '#fff' } : null}>
+      <S.CategoryLink to="/matches/basketball" style={currentPath === '/matches/basketball' ? { color: '#fff' } : undefined}>
         농구
       </S.CategoryLink>
-      <S.CategoryLink to="/matches/volleyball" style={currentPath === '/matches/volleyball' ? { color: '#fff' } : null}>
+      <S.CategoryLink to="/matches/volleyball" style={currentPath === '/matches/volleyball' ? { color: '#fff' } : undefined}>
         배구
       </S.CategoryLink>
     </S.CategoryContainer>

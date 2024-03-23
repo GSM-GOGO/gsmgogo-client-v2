19be726c68ebdd38a842d714f0f8f529c35d3b20
gjaegyun <jaegyungim06@gmail.com>
2024-03-23 16:52:00 +0900

    Change width

--- src/components/Category/style.ts
+++ src/components/Category/style.ts
@@ -2,15 +2,10 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const CategoryContainer = styled.div`
-  width: 40.5rem;
   margin-top: 1.25rem;
   margin-bottom: 2.5rem;
   display: flex;
   gap: 1.75rem;
-  @media (max-width: 675px) {
-    display: flex;
-    justify-content: center;
-  }
 `;
 
 export const CategoryLink = styled(Link)`

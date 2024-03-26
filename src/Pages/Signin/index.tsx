24b478871f04e2e1d5f905d17aed60ce105ff08f
정태관 <jxorhks86909763@gmail.com>
2024-03-27 00:26:34 +0900

    add signinLogic:

--- src/Pages/Signin/index.tsx
+++ src/Pages/Signin/index.tsx
@@ -2,33 +2,18 @@ import * as S from './style.ts';
 import { GAuthLogo, SignInLogo } from '../../assets';
 import '@msg-team/gauth-react/dist/index.css';
 
-import { useNavigate } from 'react-router-dom';
 import apiClient from '../../utils/libs/apiClient.ts';
-import { useEffect } from 'react';
-import IsLoggedIn from '../../utils/IsLoggedIn.ts';
-import { Login } from '../../utils/apis/auth.ts';
 
 const Signin = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const checkLoggedIn = async () => {
-      if (await IsLoggedIn()) {
-        navigate('/');
-      }
-    };
-    checkLoggedIn();
-  }, []);
-  Login();
-
   const handleSignIn = async () => {
     try {
       const response = await apiClient.get('/auth/login');
-      navigate(response.data);
+      window.location.href = response.data.redirect;
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
+
   return (
     <S.Wrapper>
       <S.Container>
--- src/utils/libs/apiClient.ts
+++ src/utils/libs/apiClient.ts
@@ -5,4 +5,19 @@ const apiClient = axios.create({
   withCredentials: true,
 });
 
+apiClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response && error.response.status === 403) {
+      try {
+        await axios.get(`${import.meta.env.VITE_CLIENT_API}/auth/refresh`);
+        return axios.request(error.config);
+      } catch (refreshError) {
+        window.location.href = '/signin';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default apiClient;

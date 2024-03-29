70e2834a3ce75eb7fa5f63c77e2a09d9ef6cc97a
gjaegyun <jaegyungim06@gmail.com>
2024-03-29 14:25:10 +0900

    Add leaderStyle

--- src/Pages/Register/index.tsx
+++ src/Pages/Register/index.tsx
@@ -5,6 +5,7 @@ import { Search, SmallXIcon, XIcon } from '../../assets/index.ts';
 import { useNavigate } from 'react-router-dom';
 import apiClient from '../../utils/libs/apiClient.ts';
 import Nomal from './nomal.tsx';
+import useStorePoint from '../../utils/libs/storePoint';
 
 interface SportsData {
   user_id: number;
@@ -44,6 +45,8 @@ const Register = () => {
   const [searchedName, setSearchedName] = useState('');
   const [searchResults, setSearchResults] = useState<SportsData[]>([]);
   const [selectedMembers, setSelectedMembers] = useState<SportsData[]>([]);
+  const isLeader = useStorePoint((state) => state.leader);
+  const setIsLeader = useStorePoint((state) => state.setIsLeader);
 
   const navigate = useNavigate();
 
@@ -64,6 +67,20 @@ const Register = () => {
       },
     });
   };
+
+  const debounce = (func: Function, delay: number) => {
+    let timer: NodeJS.Timeout;
+    return (...args: any[]) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
+
+  const delayedSearch = debounce((searchValue: string) => {
+    setSearchedName(searchValue);
+  }, 500);
 
   useEffect(() => {
     const getSearch = async () => {
@@ -97,6 +114,26 @@ const Register = () => {
     getSearch();
   }, [searchedName]);
 
+  useEffect(() => {
+    const getIsLeader = async () => {
+      try {
+        const token = localStorage.getItem('accessToken');
+
+        const response = await apiClient.get(`/user/is-leader`, {
+          headers: {
+            Authorization: token,
+          },
+          withCredentials: true,
+        });
+        setIsLeader(response.data.leader);
+      } catch (e) {
+        console.log('error');
+      }
+    };
+
+    getIsLeader();
+  }, []);
+
   const handleSportSelection = (sport: string) => {
     setSelectedSport(sport);
     setSelectedInput(''); // 팀 이름 input과 팀 검색 input 초기화
@@ -113,7 +150,7 @@ const Register = () => {
 
   const handleSearchNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const searchedName = e.target.value.replace(/[^\uAC00-\uD7A3]/gi, '');
-    setSearchedName(searchedName);
+    delayedSearch(searchedName);
   };
 
   const handleMemberClick = (member: SportsData) => {
@@ -155,109 +192,212 @@ const Register = () => {
             <S.CategoryContainer>
               <S.Category style={{ color: 'var(--White, #FFF)' }}>팀 추가</S.Category>
             </S.CategoryContainer>
-
-            <S.ContainerResponse>
-              <S.SubjectContainer>
-                <S.SubjectText>종목</S.SubjectText>
-                <S.SubjectBox>
-                  {eventArr.map((item) => (
-                    <S.SubjectOne onClick={() => handleSportSelection(item)} selectedSport={selectedSport === item}>
-                      <S.SubjectOneText selectedSport={selectedSport === item}>{item}</S.SubjectOneText>
-                    </S.SubjectOne>
-                  ))}
-                </S.SubjectBox>
-              </S.SubjectContainer>
-              {selectedSport !== '일반경기' ? (
-                <>
-                  <S.TeamInputContainer>
-                    <S.SubjectText>팀 이름</S.SubjectText>
-
-                    <S.TeamInputBox>
-                      <S.TeamInput
-                        type="text"
-                        placeholder="팀 이름은 최대 7글자 입니다"
-                        value={teamName}
-                        onChange={handleTeamNameChange}
-                      />
-                      <S.TeamInputText style={{ color: 'var(--White, #FFF)' }}>팀</S.TeamInputText>
-                    </S.TeamInputBox>
-                  </S.TeamInputContainer>
-
-                  <S.TeamInputContainer>
-                    <S.SubjectText>팀원</S.SubjectText>
-                    {dividedSelectedMembers.map((group, groupIndex) => (
-                      <S.MemberSelected key={groupIndex}>
-                        {group.map((member, index) => (
-                          <S.MemberSelectList key={index}>
-                            <S.MemberName>{member.user_name}</S.MemberName>
-                            <div
-                              style={{ cursor: 'pointer', display: 'flex' }}
-                              onClick={() => handleRemoveMember(member)}
-                            >
-                              <XIcon />
-                            </div>
-                          </S.MemberSelectList>
-                        ))}
-                      </S.MemberSelected>
+            {isLeader === true ? (
+              <S.ContainerResponse>
+                <S.SubjectContainer>
+                  <S.SubjectText>종목</S.SubjectText>
+                  <S.SubjectBox>
+                    {eventArr.map((item, i) => (
+                      <S.SubjectOne
+                        key={i}
+                        onClick={() => handleSportSelection(item)}
+                        selectedSport={selectedSport === item}
+                      >
+                        <S.SubjectOneText selectedSport={selectedSport === item}>{item}</S.SubjectOneText>
+                      </S.SubjectOne>
                     ))}
+                  </S.SubjectBox>
+                </S.SubjectContainer>
+                {selectedSport !== '일반경기' ? (
+                  <>
+                    <S.TeamInputContainer>
+                      <S.SubjectText>팀 이름</S.SubjectText>
 
-                    <S.TeamInputBox>
-                      <S.TeamInput type="text" placeholder="이름으로 검색하세요" onChange={handleSearchNameChange} />
-                      <div style={{ cursor: 'pointer' }}>
-                        <Search />
-                      </div>
-                    </S.TeamInputBox>
-                  </S.TeamInputContainer>
-                  {searchedName !== '' && searchResults.length > 0 && (
-                    <S.overScroll style={{ height: '22.5em' }}>
-                      {searchResults.map((result, index) => (
-                        <S.MapTeamMember key={index} onClick={() => handleMemberClick(result)}>
-                          <S.MemberName>
-                            {Number[result.user_grade]}학년{Number[result.user_class]}반 {result.user_name}
-                          </S.MemberName>
-                        </S.MapTeamMember>
+                      <S.TeamInputBox>
+                        <S.TeamInput
+                          type="text"
+                          placeholder="팀 이름은 최대 7글자 입니다"
+                          value={teamName}
+                          onChange={handleTeamNameChange}
+                        />
+                        <S.TeamInputText style={{ color: 'var(--White, #FFF)' }}>팀</S.TeamInputText>
+                      </S.TeamInputBox>
+                    </S.TeamInputContainer>
+
+                    <S.TeamInputContainer>
+                      <S.SubjectText>팀원</S.SubjectText>
+                      {dividedSelectedMembers.map((group, groupIndex) => (
+                        <S.MemberSelected key={groupIndex}>
+                          {group.map((member, index) => (
+                            <S.MemberSelectList key={index}>
+                              <S.MemberName>{member.user_name}</S.MemberName>
+                              <div
+                                style={{ cursor: 'pointer', display: 'flex' }}
+                                onClick={() => handleRemoveMember(member)}
+                              >
+                                <XIcon />
+                              </div>
+                            </S.MemberSelectList>
+                          ))}
+                        </S.MemberSelected>
                       ))}
-                    </S.overScroll>
-                  )}
-                </>
-              ) : (
-                <Nomal />
-              )}
 
-              <div
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  marginTop: '20rem',
-                }}
-              >
-                <S.FormationBtn
-                  disabled={selectedMembers.length !== MAX_MEMBERS[selectedSport] || teamName === ''}
-                  onClick={goSports}
+                      <S.TeamInputBox>
+                        <S.TeamInput type="text" placeholder="이름으로 검색하세요" onChange={handleSearchNameChange} />
+                        <div style={{ cursor: 'pointer' }}>
+                          <Search />
+                        </div>
+                      </S.TeamInputBox>
+                    </S.TeamInputContainer>
+                    {searchedName !== '' && searchResults.length > 0 && (
+                      <S.overScroll style={{ height: '22.5em' }}>
+                        {searchResults.map((result, index) => (
+                          <S.MapTeamMember key={index} onClick={() => handleMemberClick(result)}>
+                            <S.MemberName>
+                              {Number[result.user_grade]}학년{Number[result.user_class]}반 {result.user_name}
+                            </S.MemberName>
+                          </S.MapTeamMember>
+                        ))}
+                      </S.overScroll>
+                    )}
+                  </>
+                ) : (
+                  <Nomal />
+                )}
+
+                <div
                   style={{
-                    backgroundColor:
-                      selectedMembers.length === MAX_MEMBERS[selectedSport] && teamName !== ''
-                        ? 'var(--Main, #23F69A)'
-                        : undefined,
-                    cursor:
-                      selectedMembers.length === MAX_MEMBERS[selectedSport] && teamName !== ''
-                        ? 'pointer'
-                        : 'not-allowed',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    marginTop: '20rem',
                   }}
                 >
-                  <S.FormationText
+                  <S.FormationBtn
+                    disabled={selectedMembers.length !== MAX_MEMBERS[selectedSport] || teamName === ''}
+                    onClick={goSports}
                     style={{
-                      color:
+                      backgroundColor:
                         selectedMembers.length === MAX_MEMBERS[selectedSport] && teamName !== ''
-                          ? 'var(--Black, #1C1C1F)'
-                          : 'undefined',
+                          ? 'var(--Main, #23F69A)'
+                          : undefined,
+                      cursor:
+                        selectedMembers.length === MAX_MEMBERS[selectedSport] && teamName !== ''
+                          ? 'pointer'
+                          : 'not-allowed',
                     }}
                   >
-                    {selectedSport !== '일반경기' ? '포메이션 짜기' : '등록하기'}
-                  </S.FormationText>
-                </S.FormationBtn>
-              </div>
-            </S.ContainerResponse>
+                    <S.FormationText
+                      style={{
+                        color:
+                          selectedMembers.length === MAX_MEMBERS[selectedSport] && teamName !== ''
+                            ? 'var(--Black, #1C1C1F)'
+                            : 'undefined',
+                      }}
+                    >
+                      {selectedSport !== '일반경기' ? '포메이션 짜기' : '등록하기'}
+                    </S.FormationText>
+                  </S.FormationBtn>
+                </div>
+              </S.ContainerResponse>
+            ) : (
+              <S.ContainerResponse>
+                <S.SubjectContainer>
+                  <S.SubjectText>종목</S.SubjectText>
+                  <S.SubjectBox>
+                    <S.SubjectOne
+                      onClick={() => handleSportSelection('배드민턴')}
+                      selectedSport={selectedSport === '배드민턴'}
+                    >
+                      <S.SubjectOneText selectedSport={selectedSport === '배드민턴'}>배드민턴</S.SubjectOneText>
+                    </S.SubjectOne>
+                  </S.SubjectBox>
+                </S.SubjectContainer>
+                <S.TeamInputContainer>
+                  <S.SubjectText>팀 이름</S.SubjectText>
+
+                  <S.TeamInputBox>
+                    <S.TeamInput
+                      type="text"
+                      placeholder="팀 이름은 최대 7글자 입니다"
+                      value={teamName}
+                      onChange={handleTeamNameChange}
+                    />
+                    <S.TeamInputText style={{ color: 'var(--White, #FFF)' }}>팀</S.TeamInputText>
+                  </S.TeamInputBox>
+                </S.TeamInputContainer>
+
+                <S.TeamInputContainer>
+                  <S.SubjectText>팀원</S.SubjectText>
+                  {dividedSelectedMembers.map((group, groupIndex) => (
+                    <S.MemberSelected key={groupIndex}>
+                      {group.map((member, index) => (
+                        <S.MemberSelectList key={index}>
+                          <S.MemberName>{member.user_name}</S.MemberName>
+                          <div
+                            style={{ cursor: 'pointer', display: 'flex' }}
+                            onClick={() => handleRemoveMember(member)}
+                          >
+                            <XIcon />
+                          </div>
+                        </S.MemberSelectList>
+                      ))}
+                    </S.MemberSelected>
+                  ))}
+
+                  <S.TeamInputBox>
+                    <S.TeamInput type="text" placeholder="이름으로 검색하세요" onChange={handleSearchNameChange} />
+                    <div style={{ cursor: 'pointer' }}>
+                      <Search />
+                    </div>
+                  </S.TeamInputBox>
+                </S.TeamInputContainer>
+                {searchedName !== '' && searchResults.length > 0 && (
+                  <S.overScroll style={{ height: '22.5em' }}>
+                    {searchResults.map((result, index) => (
+                      <S.MapTeamMember key={index} onClick={() => handleMemberClick(result)}>
+                        <S.MemberName>
+                          {Number[result.user_grade]}학년{Number[result.user_class]}반 {result.user_name}
+                        </S.MemberName>
+                      </S.MapTeamMember>
+                    ))}
+                  </S.overScroll>
+                )}
+
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    marginTop: '20rem',
+                  }}
+                >
+                  <S.FormationBtn
+                    disabled={selectedMembers.length !== MAX_MEMBERS[selectedSport] || teamName === ''}
+                    onClick={goSports}
+                    style={{
+                      backgroundColor:
+                        selectedMembers.length === MAX_MEMBERS[selectedSport] && teamName !== ''
+                          ? 'var(--Main, #23F69A)'
+                          : undefined,
+                      cursor:
+                        selectedMembers.length === MAX_MEMBERS[selectedSport] && teamName !== ''
+                          ? 'pointer'
+                          : 'not-allowed',
+                    }}
+                  >
+                    <S.FormationText
+                      style={{
+                        color:
+                          selectedMembers.length === MAX_MEMBERS[selectedSport] && teamName !== ''
+                            ? 'var(--Black, #1C1C1F)'
+                            : 'undefined',
+                      }}
+                    >
+                      {selectedSport !== '일반경기' ? '포메이션 짜기' : '등록하기'}
+                    </S.FormationText>
+                  </S.FormationBtn>
+                </div>
+              </S.ContainerResponse>
+            )}
           </S.ContainerResponse>
         </S.Container>
       </S.Wrapper>

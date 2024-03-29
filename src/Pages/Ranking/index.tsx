2f11eb55ce42fbf7994488c0786f5cc82ae1d269
happytaeyoon <s23021@gsm.hs.kr>
2024-03-30 04:58:54 +0900

    Add RankApi

--- src/Pages/Ranking/index.tsx
+++ src/Pages/Ranking/index.tsx
@@ -5,99 +5,95 @@ import apiClient from '../../utils/libs/apiClient';
 import { Category, CategoryContainer } from '../Formation/style';
 import * as S from './style';
 import useAccessTokenCheck from '../../hook/useAccessTokenCheck';
-
-interface Data {
-  name: string;
-  point: number;
-  isMe?: boolean;
-}
-
-const dataArray: Data[] = [
-  {
-    name: '1101 김순자',
-    point: 10000,
-    isMe: true,
-  },
-  {
-    name: '1102 김덕자',
-    point: 11000,
-  },
-  {
-    name: '1105 김감자',
-    point: 1900,
-  },
-  {
-    name: '1106 김승자',
-    point: 20000,
-  },
-];
-
-const topThreeData = dataArray.sort((a, b) => b.point - a.point).slice(0, 3);
-
-const firstPlacePoints = topThreeData[0].point;
-const secondPlacePoints = topThreeData[1].point;
-const thirdPlacePoints = topThreeData[2].point;
-
-const secondPlaceHeight = (260 / firstPlacePoints) * secondPlacePoints;
-const thirdPlaceHeight = (260 / firstPlacePoints) * thirdPlacePoints;
-
-const getRankInfo = (
-  index: number,
-  data: {
-    name: string;
-    point: { toLocaleString: () => string };
-  }[]
-) => {
-  const rankInfo = {
-    rankName: '',
-    rankPoint: '',
-    rankComponent: null as React.ReactNode,
-    rank: '',
-  };
-  rankInfo.rankName = index === 0 ? data[2].name : index === 1 ? data[0].name : data[1].name;
-  rankInfo.rankPoint =
-    index === 0
-      ? data[2].point.toLocaleString()
-      : index === 1
-        ? data[0].point.toLocaleString()
-        : data[1].point.toLocaleString();
-  rankInfo.rankComponent =
-    index === 0 ? (
-      <RankBar height={thirdPlaceHeight} />
-    ) : index === 1 ? (
-      <RankBar height={260} />
-    ) : (
-      <RankBar height={secondPlaceHeight} />
-    );
-  rankInfo.rank = index === 0 ? '3등' : index === 1 ? '1등' : '2등';
-  return rankInfo;
-};
+import { useEffect, useState } from 'react';
 
 const Ranking = () => {
   useAccessTokenCheck();
-  // const token = localStorage.getItem('refreshToken');
-  // const config = {
-  //   headers: {
-  //     'Refresh-Token': `${token}`,
-  //   },
-  // };
 
-  // const handleTest = async () => {
-  //   try {
-  //     const Response = await apiClient.get(`/auth/refresh`, {
-  //       headers: {
-  //         'Refresh-Token': `${token}`,
-  //       },
-  //     });
-  //     const accessToken = Response.headers['authorization'];
-  //     const refreshToken = Response.headers['refresh-token'];
-  //     console.log(accessToken);
-  //     console.log(refreshToken);
-  //     // return axios.request();
-  //   } catch (refreshError) {
-  //     // window.location.href = '/signin';
-  //   }
-  // };
+  const [RankData, setRankData] = useState<any[]>([]);
+  const [myId, setMyId] = useState(null); 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem('accessToken');
+        const response = await apiClient.get(`/rank`, {
+          headers: {
+            Authorization: `${token}`,
+          },
+        });
+        console.log(response.data);
+        setRankData(response.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchData();
+  }, []);
+  useEffect(() => {
+    const fetchMyId = async () => {
+      try {
+        const token = localStorage.getItem('accessToken');
+        const response = await apiClient.get(`/user/my-id`, {
+          headers: {
+            Authorization: `${token}`,
+          },
+        });
+        console.log(response.data);
+        setMyId(response.data.user_id);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchMyId();
+  }, []);
+
+
+  const topThreeDatatest = RankData.length > 0 ? RankData.sort((a, b) => b.user_point - a.user_point).slice(0, 3) : [];
+
+  const firstPlacePointstest = topThreeDatatest.length > 0 ? topThreeDatatest[0].user_point : 0;
+  const secondPlacePointstest = topThreeDatatest.length > 1 ? topThreeDatatest[1].user_point : 0;
+  const thirdPlacePointstest = topThreeDatatest.length > 2 ? topThreeDatatest[2].user_point : 0;
+
+  const secondPlaceHeighttest = (260 / firstPlacePointstest) * secondPlacePointstest;
+  const thirdPlaceHeighttest = (260 / firstPlacePointstest) * thirdPlacePointstest;
+
+  const getRankInfotest = (
+    index: number,
+    data: {
+      user_name: string;
+      user_point: { toLocaleString: () => string };
+    }[]
+  ) => {
+    const rankInfotest = {
+      rankName: '',
+      rankPoint: '',
+      rankComponent: null as React.ReactNode,
+      rank: '',
+    };
+  
+    if (index === 0 && data.length > 2) {
+      rankInfotest.rankName = data[2].user_name;
+      rankInfotest.rankPoint = data[2].user_point.toLocaleString();
+      rankInfotest.rankComponent = <RankBar height={thirdPlaceHeighttest} />;
+      rankInfotest.rank = '3등';
+    } else if (index === 1 && data.length > 0) {
+      rankInfotest.rankName = data[0].user_name;
+      rankInfotest.rankPoint = data[0].user_point.toLocaleString();
+      rankInfotest.rankComponent = <RankBar height={260} />;
+      rankInfotest.rank = '1등';
+    } else if (index === 2 && data.length > 1) {
+      rankInfotest.rankName = data[1].user_name;
+      rankInfotest.rankPoint = data[1].user_point.toLocaleString();
+      rankInfotest.rankComponent = <RankBar height={secondPlaceHeighttest} />;
+      rankInfotest.rank = '2등';
+    }
+  
+    return rankInfotest;
+  };
+  
+
   return (
     <>
       <HeaderContainer />
@@ -108,12 +104,11 @@ const Ranking = () => {
           </CategoryContainer>
           <S.MainContainer>
             <S.LankWrapper>
-              {dataArray
-                .sort((a, b) => b.point - a.point)
+              {RankData.sort((a, b) => b.user_point - a.user_point)
                 .slice(0, 3)
-                .sort((a, b) => (a.name > b.name ? 1 : -1))
+                .sort((a, b) => (a.user_name > b.user_name ? 1 : -1))
                 .map((item, index) => {
-                  const { rankName, rankPoint, rankComponent, rank } = getRankInfo(index, dataArray);
+                  const { rankName, rankPoint, rankComponent, rank } = getRankInfotest(index, RankData);
                   return (
                     <S.LankContainer key={index}>
                       <S.Name>{rankName}</S.Name>
@@ -133,39 +128,37 @@ const Ranking = () => {
                 })}
             </S.LankWrapper>
             <S.ListWrapper>
-              {dataArray.map((item, index) => (
+              {RankData.map((item, index) => (
                 <>
-                  {item.isMe && (
+                  {item.user_id==myId && (
                     <S.List key={index}>
                       <S.TextContainer>
                         <S.Text>
                           <S.Lank rank={index < 3}>{index + 1}등</S.Lank>
-                          <S.Name>{item.name}</S.Name>
+                          <S.Name>{item.user_name}</S.Name>
                         </S.Text>
                       </S.TextContainer>
                       <S.Text>
-                        <S.Point>{item.point.toLocaleString()}P</S.Point>
+                        <S.Point>{item.user_point.toLocaleString()}P</S.Point>
                       </S.Text>
                     </S.List>
                   )}
                 </>
               ))}
               <S.Stroke />
-              {dataArray
-                .sort((a, b) => b.point - a.point)
-                .map((item, index) => (
-                  <S.List key={index}>
-                    <S.TextContainer>
-                      <S.Text>
-                        <S.Lank rank={index < 3}>{index + 1}등</S.Lank>
-                        <S.Name>{item.name}</S.Name>
-                      </S.Text>
-                    </S.TextContainer>
+              {RankData.sort((a, b) => b.user_point - a.user_point).map((item, index) => (
+                <S.List key={index}>
+                  <S.TextContainer>
                     <S.Text>
-                      <S.Point>{item.point.toLocaleString()}P</S.Point>
+                      <S.Lank rank={index < 3}>{index + 1}등</S.Lank>
+                      <S.Name>{item.user_name}</S.Name>
                     </S.Text>
-                  </S.List>
-                ))}
+                  </S.TextContainer>
+                  <S.Text>
+                    <S.Point>{item.user_point.toLocaleString()}P</S.Point>
+                  </S.Text>
+                </S.List>
+              ))}
             </S.ListWrapper>
           </S.MainContainer>
         </S.Container>

435aebf1d81b60bcb040c0daec021901136c88d3
gjaegyun <jaegyungim06@gmail.com>
2024-03-31 12:48:49 +0900

    HotFix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { CookiesProvider } from 'react-cookie';
-
+//
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <CookiesProvider>
     <App />

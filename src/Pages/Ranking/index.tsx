421f8384c9f7b359312f982fe1cea6feeb7ecc22
happytaeyoon <s23021@gsm.hs.kr>
2024-04-01 16:29:07 +0900

    RankGradeClass

--- src/Pages/Ranking/index.tsx
+++ src/Pages/Ranking/index.tsx
@@ -54,12 +54,16 @@ const Ranking = () => {
     index: number,
     data: {
       user_name: string;
+      user_grade: string;
+      user_class: string;
       user_point: { toLocaleString: () => string };
     }[]
   ) => {
     const rankInfotest = {
       rankName: '',
       rankPoint: '',
+      rankGrade: '',
+      rankClass: '',
       rankComponent: null as React.ReactNode,
       rank: '',
     };
@@ -67,21 +71,38 @@ const Ranking = () => {
     if (index === 0 && data.length > 2) {
       rankInfotest.rankName = data[2].user_name;
       rankInfotest.rankPoint = data[2].user_point.toLocaleString();
+      rankInfotest.rankGrade = data[2].user_grade;
+      rankInfotest.rankClass = data[2].user_class;
       rankInfotest.rankComponent = <RankBar height={thirdPlaceHeighttest} />;
       rankInfotest.rank = '3등';
     } else if (index === 1 && data.length > 0) {
       rankInfotest.rankName = data[0].user_name;
       rankInfotest.rankPoint = data[0].user_point.toLocaleString();
+      rankInfotest.rankGrade = data[0].user_grade;
+      rankInfotest.rankClass = data[0].user_class;
       rankInfotest.rankComponent = <RankBar height={260} />;
       rankInfotest.rank = '1등';
     } else if (index === 2 && data.length > 1) {
       rankInfotest.rankName = data[1].user_name;
       rankInfotest.rankPoint = data[1].user_point.toLocaleString();
+      rankInfotest.rankGrade = data[1].user_grade;
+      rankInfotest.rankClass = data[1].user_class;
       rankInfotest.rankComponent = <RankBar height={secondPlaceHeighttest} />;
       rankInfotest.rank = '2등';
     }
 
     return rankInfotest;
+  };
+
+  type Numbertype = {
+    [key: string]: number;
+  };
+
+  const Number: Numbertype = {
+    ONE: 1,
+    TWO: 2,
+    THREE: 3,
+    FOUR: 4,
   };
 
   return (
@@ -98,10 +119,10 @@ const Ranking = () => {
                 .slice(0, 3)
                 .sort((a, b) => (a.user_name > b.user_name ? 1 : -1))
                 .map((_, index) => {
-                  const { rankName, rankPoint, rankComponent, rank } = getRankInfotest(index, RankData);
+                  const { rankName, rankPoint,rankClass,rankGrade, rankComponent, rank } = getRankInfotest(index, RankData);
                   return (
                     <S.LankContainer key={index}>
-                      <S.Name>{rankName}</S.Name>
+                      <S.Name> {Number[rankGrade]}-{Number[rankClass]} {rankName}</S.Name>
                       <S.Point>{rankPoint}P</S.Point>
                       {rankComponent}
                       <S.Name
@@ -125,7 +146,10 @@ const Ranking = () => {
                       <S.TextContainer>
                         <S.Text>
                           <S.Lank rank={index < 3}>{index + 1}등</S.Lank>
-                          <S.Name>{item.user_name}</S.Name>
+                          <S.Name>
+                            {' '}
+                            {Number[item.user_grade]}학년{Number[item.user_class]}반 {item.user_name}
+                          </S.Name>
                         </S.Text>
                       </S.TextContainer>
                       <S.Text>
@@ -141,7 +165,10 @@ const Ranking = () => {
                   <S.TextContainer>
                     <S.Text>
                       <S.Lank rank={index < 3}>{index + 1}등</S.Lank>
-                      <S.Name>{item.user_name}</S.Name>
+                      <S.Name>
+                        {' '}
+                        {Number[item.user_grade]}학년{Number[item.user_class]}반 {item.user_name}
+                      </S.Name>
                     </S.Text>
                   </S.TextContainer>
                   <S.Text>

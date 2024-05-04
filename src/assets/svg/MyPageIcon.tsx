1b8f222098ff6601765732b900e30942ec28d5ff
정태관 <jxorhks86909763@gmail.com>
2024-05-04 23:46:53 +0900

    update MyPageIcon camelcase

--- src/assets/svg/MyPageIcon.tsx
+++ src/assets/svg/MyPageIcon.tsx
@@ -3,8 +3,8 @@ const MyPageIcon = () => {
     <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect y="0.5" width="24" height="24" rx="12" fill="#23F69A" />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M4 21.4444V20.5C4 17.84 9.33 16.5 12 16.5C14.67 16.5 20 17.84 20 20.5V21.4444C17.877 23.3446 15.0734 24.5 12 24.5C8.92658 24.5 6.12303 23.3446 4 21.4444ZM16 10.5C16 12.71 14.21 14.5 12 14.5C9.79 14.5 8 12.71 8 10.5C8 8.29 9.79 6.5 12 6.5C14.21 6.5 16 8.29 16 10.5Z"
         fill="#1C1C1F"
       />

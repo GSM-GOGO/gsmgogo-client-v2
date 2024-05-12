82a7a38a2d68206cbf2978d3442a5bf6190f4507
gjaegyun <jaegyungim06@gmail.com>
2024-05-12 14:39:55 +0900

    Add getBtnClicked

--- src/Pages/Minigame/NunchiGame/index.tsx
+++ src/Pages/Minigame/NunchiGame/index.tsx
@@ -1,14 +1,46 @@
 import * as S from './style';
 import { ToastContainer, toast } from 'react-toastify';
 import { Toaster } from 'react-hot-toast';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, Button_click } from '../../../assets/index';
 import MiniGameCategory from '../../../components/MiniGameCategory';
+import apiClient from '../../../utils/libs/apiClient';
 
 const NunchiGame = () => {
-  const [clickedButton, setClickedButton] = useState(0);
+  type ClickResponse = {
+    button_type: 'ONE' | 'TWO' | 'THREE' | 'FOUR' | 'FIVE' | null;
+  };
 
-  const handleButtonClick = (button: number) => setClickedButton(clickedButton === button ? 0 : button);
+  const [clickedButton, setClickedButton] = useState(0);
+  const [isClicked, setIsClicked] = useState(false);
+  const [clickResponse, setClickResponse] = useState<ClickResponse>();
+
+  useEffect(() => {
+    getUserBtnClicked();
+  }, []);
+
+  const getUserBtnClicked = async () => {
+    try {
+      const token = localStorage.getItem('accessToken');
+
+      const response = await apiClient.get(`/game/button`, {
+        headers: {
+          Authorization: token,
+        },
+        withCredentials: true,
+      });
+      setClickResponse(response.data.button_type);
+    } catch (e) {}
+  };
+
+  const handleButtonClick = (button: number) => {
+    setClickedButton(clickedButton === button ? 0 : button);
+  };
+
+  const pressBtn = (clickBtn: number, isClicked: boolean) => {
+    setIsClicked(true);
+    console.log(clickBtn, isClicked);
+  };
 
   const buttonPositions = [
     { gridRow: 1, gridColumn: 1 },
@@ -27,6 +59,7 @@ const NunchiGame = () => {
             <S.ButtonContainer>
               {[1, 2, 3, 4, 5].map((number, index) => (
                 <div
+                  key={index}
                   style={{
                     gridRow: buttonPositions[number - 1].gridRow,
                     gridColumn: buttonPositions[number - 1].gridColumn,
@@ -37,7 +70,6 @@ const NunchiGame = () => {
                     src={clickedButton === number ? Button_click : Button}
                     alt={`Button${index + 1}`}
                     onClick={() => handleButtonClick(number)}
-                    key={index}
                   />
                   <S.ButtonText>{`${index + 1}번`}</S.ButtonText>
                 </div>
@@ -55,14 +87,27 @@ const NunchiGame = () => {
                   </div>
                 ))}
               </S.InfoContainer>
-              <S.Button color={clickedButton}>
-                {clickedButton !== 0 && `${clickedButton}번 `}
-                버튼 누르기
-              </S.Button>
-              <S.Text>
-                매일 밤 11시, 가장 적게 눌린 버튼을 누른 분들에게 포인트를 지급해요 <br />
-                <span>50만 원 이상 보유자는 참여할 수 없어요</span>
-              </S.Text>
+              {!isClicked && (
+                <>
+                  <S.Button color={clickedButton} onClick={() => pressBtn(clickedButton, isClicked)}>
+                    {clickedButton !== 0 && `${clickedButton}번 `}
+                    버튼 누르기
+                  </S.Button>
+                  <S.Text>
+                    매일 밤 11시, 가장 적게 눌린 버튼을 누른 분들에게 포인트를 지급해요 <br />
+                    <span>50만 원 이상 보유자는 참여할 수 없어요</span>
+                  </S.Text>
+                </>
+              )}
+              {isClicked && (
+                <>
+                  <S.Button color={0}>{clickResponse && `${clickResponse.button_type}번 버튼을 눌렀어요`}</S.Button>
+                  <S.Text>
+                    매일 밤 11시, 가장 적게 눌린 버튼을 누른 분들에게 포인트를 지급해요 <br />
+                    <span>50만 원 이상 보유자는 참여할 수 없어요</span>
+                  </S.Text>
+                </>
+              )}
             </S.BottomContainer>
           </S.ContainerResponse>
         </S.Container>
